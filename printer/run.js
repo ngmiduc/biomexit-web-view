cdb347a4e3f02dfa1ac242084154870e725be968
Minh Duc Nguyen <nguyenminhducdd@yahoo.de>
2019-09-06 09:02:38 +0000

    fix

--- printer/run.js
+++ printer/run.js
@@ -10,7 +10,6 @@ const escpos = require("escpos")
 
 const sharp = require("sharp")
 
-const http = require("https")
 const fs = require("fs")
 
 const cerd = require("./cred.json")
@@ -72,95 +71,100 @@ firestore
     console.log("")
     console.log("[get data] GET new data from tracking ...")
 
-    let today = new Date()
-    today.setTime(today.getTime() + 1 * 86400000)
-    today = today.toISOString()
-    today = today.split("T")[0]
+    if (!BUSY) {
+      BUSY = true
 
-    //add bucket get image  OPTIONAL
-    // e.g.: URL is https://firebasestorage.googleapis.com/v0/b/biomexit.appspot.com/o/faces%2F583.jpg?alt=media&token=b662d5ec-259c-4851-8d50-4bea52dfcb1a
-    let fileName = item.url.split("iomexit.appspot.com/o/faces%2")[1]
-    fileName = fileName.split(".jpg")[0]
-    fileName = "faces/" + fileName + ".jpg"
-    const file = bucket.file(fileName)
-    console.log("FILE : " + fileName)
-    console.log("FILE : " + file)
-    return file
-      .getSignedUrl({
-        action: "read",
-        expires: today
-      })
-      .then(signedUrls => {
-        // signedUrls[0] contains the file's public URL
+      let today = new Date()
+      let valid = new Date()
+      valid.setTime(valid.getTime() + 1 * 86400000)
+      valid = valid.toISOString()
+      valid = valid.split("T")[0]
 
-        const sURL = signedUrls[0]
+      //add bucket get image  OPTIONAL
+      // e.g.: URL is https://firebasestorage.googleapis.com/v0/b/biomexit.appspot.com/o/faces%2F583.jpg?alt=media&token=b662d5ec-259c-4851-8d50-4bea52dfcb1a
+      let fileName = item.url.split("iomexit.appspot.com/o/faces%2")[1]
+      fileName = fileName.split(".jpg")[0]
+      fileName = "faces/" + fileName + ".jpg"
+      const file = bucket.file(fileName)
+      console.log("FILE : " + fileName)
+      console.log("FILE : " + file)
+      return file
+        .getSignedUrl({
+          action: "read",
+          expires: valid
+        })
+        .then(signedUrls => {
+          // signedUrls[0] contains the file's public URL
 
-        console.log("URL : " + sURL)
+          const sURL = signedUrls[0]
 
-        fs.unlink("file.png", async function(err) {
-          if (err) throw err
-          // if no error, file has been deleted successfully
-          console.log("File deleted!")
+          console.log("[data fetching] get signed URL : " + sURL)
+          console.log("[data fetching] valid until : " + valid)
 
-          // var request = require("request")
-          // var download = function(uri, filename, callback) {
-          //   request.head(uri, function(err, res, body) {
-          //     console.log("content-type:", res.headers["content-type"])
-          //     console.log("content-length:", res.headers["content-length"])
-          //
-          //     request(uri)
-          //       .pipe(fs.createWriteStream(filename))
-          //       .on("close", callback)
-          //   })
-          // }
-
-          // await file.download({
-          //   destination: "file.jpg"
-          // })
-
-          // download(sURL, "file.jpg", function() {
-          // console.log("GOT IMAGE")
-
-          async function downloadImage() {
-            const url = item.url
-            const path = "file.png"
-            const writer = fs.createWriteStream(path)
-
-            const response = await axios({
-              url,
-              method: "GET",
-              responseType: "stream"
-            })
-
-            response.data.pipe(writer)
-
-            return new Promise((resolve, reject) => {
-              writer.on("finish", resolve)
-              writer.on("error", reject)
-            })
-          }
-
-          await downloadImage()
-
-          fs.unlink("file2.png", async function(err) {
+          fs.unlink("file.png", async function(err) {
             if (err) throw err
+            // if no error, file has been deleted successfully
+            console.log("[file server] old file deleted!")
 
-            let inputFile = "file.png"
-            let outputFile = "file2.png"
+            // var request = require("request")
+            // var download = function(uri, filename, callback) {
+            //   request.head(uri, function(err, res, body) {
+            //     console.log("content-type:", res.headers["content-type"])
+            //     console.log("content-length:", res.headers["content-length"])
+            //
+            //     request(uri)
+            //       .pipe(fs.createWriteStream(filename))
+            //       .on("close", callback)
+            //   })
+            // }
 
-            sharp(inputFile)
-              .resize({ width: 500, height: 300 })
-              .grayscale()
-              .toFile(outputFile)
-              .then(function() {
-                console.log("Success : FILE RESIZED")
+            // await file.download({
+            //   destination: "file.jpg"
+            // })
 
-                console.log("URL is " + item.url)
+            // download(sURL, "file.jpg", function() {
+            // console.log("GOT IMAGE")
 
-                console.log("[get data] timedate: ", today)
+            async function downloadImage() {
+              const url = item.url
+              const path = "file.png"
+              const writer = fs.createWriteStream(path)
 
-                if (!BUSY) {
-                  BUSY = true
+              const response = await axios({
+                url,
+                method: "GET",
+                responseType: "stream"
+              })
+
+              response.data.pipe(writer)
+
+              return new Promise((resolve, reject) => {
+                writer.on("finish", resolve)
+                writer.on("error", reject)
+              })
+            }
+
+            await downloadImage()
+            console.log("[file server] set new file")
+
+            fs.unlink("file2.png", async function(err) {
+              if (err) throw err
+
+              console.log("[file server] delete old file (2)")
+              let inputFile = "file.png"
+              let outputFile = "file2.png"
+
+              sharp(inputFile)
+                .resize({ width: 500, height: 300 })
+                .grayscale()
+                .toFile(outputFile)
+                .then(function() {
+                  console.log("[file server] save new file (2)")
+                  console.log("[file server] FILE RESIZED")
+
+                  console.log("[file server] URL is " + item.url)
+
+                  console.log("[get data] timedate: ", today)
 
                   const ESCPOSImageProcessor = require("escpos-image-processor")
 
@@ -174,7 +178,7 @@ firestore
                     .then(path => {
                       if (path) {
                         console.log(
-                          `Processed image saved to ${path}, printing...`
+                          `[file server] Processed image saved to ${path}, printing...`
                         )
 
                         // processor.print(device, printer)
@@ -185,6 +189,8 @@ firestore
                       const tux = "file2.png"
                       escpos.Image.load(tux, function(image) {
                         device.open(async function() {
+                          console.log("[printer] open printer ...")
+
                           let state = [
                             "single",
                             "married",
@@ -207,7 +213,7 @@ firestore
                           })
                           console.log("[get data] code: ", barcode)
 
-                          for (var i = 0; i < 10; i++) {
+                          for (var i = 0; i < 5; i++) {
                             console.log(
                               "[fetching ...] " +
                                 Math.floor(
@@ -220,9 +226,11 @@ firestore
                           await printer.align("ct")
                           await printer.style("bu")
                           await printer.size(1, 1)
+                          await printer.image(image, "s8")
+                          await printer.text("")
+                          await printer.text("BIOMETRIC EXIT")
                           await printer.text("")
                           await printer.text("TIME " + today)
-                          await printer.text("BIOMETRIC EXIT")
                           await printer.text("ID " + item.id)
                           await printer.text("ANALYSIS " + item.analysis)
                           await printer.text(
@@ -232,8 +240,6 @@ firestore
                           await printer.text("AGE: " + age)
                           await printer.barcode("" + barcode, "EAN13")
                           await printer.text("")
-
-                          await printer.image(image, "s8")
 
                           // await printer.image(image, "d8")
                           // await printer.image(image, "s24")
@@ -250,13 +256,13 @@ firestore
                       })
                     })
                     .catch(error => console.error(error))
-                } else {
-                  console.log("[BLOCK DATA]")
-                  console.log("[BLOCK DATA]")
-                  console.log("[BLOCK DATA]")
-                }
-              })
+                })
+            })
           })
         })
-      })
+    } else {
+      console.log("[BLOCK DATA]")
+      console.log("[BLOCK DATA]")
+      console.log("[BLOCK DATA]")
+    }
   })
